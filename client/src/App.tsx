7c5a2465eeb383c71d8d3db10391e8a881a758a6
KimJiyun <magi83105@gmail.com>
2024-02-19 15:55:34 +0900

    feat: 라우팅

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,24 @@
 import './utils/variables.scss';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import HomeTemplate from './components/template/HomeTemplate';
+
+import LeftNavBar from './components/organism/LeftNavBar';
+import TopBar from './components/organism/TopBar';
+import MainBox from './components/atom/MainBox';
+import ProductListBox from './components/organism/ProductListBox';
+import LoginArea from './components/template/LoginArea';
+import JoinArea from './components/template/JoinArea';
 
 function App() {
   return (
     <BrowserRouter>
       <div className='App'>
-        <HomeTemplate />
+        <LeftNavBar />
+        <TopBar />
+        <Routes>
+          <Route path='/' element={<MainBox children={ProductListBox()} />} />
+          <Route path='/login' element={<MainBox children={LoginArea()} />} />
+          <Route path='/join' element={<MainBox children={JoinArea()} />} />
+        </Routes>
       </div>
     </BrowserRouter>
   );
