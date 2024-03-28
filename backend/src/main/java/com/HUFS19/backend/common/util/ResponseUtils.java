f56c3b74b9b682198803d95d0c1e8091a8090b9b
JH CHO <junh9541@gmail.com>
2024-03-28 20:44:53 +0900

    fix: generic 관련 warning 해결

--- backend/src/main/java/com/HUFS19/backend/common/util/ResponseUtils.java
+++ backend/src/main/java/com/HUFS19/backend/common/util/ResponseUtils.java
@@ -2,19 +2,18 @@ package com.HUFS19.backend.common.util;
 
 import com.HUFS19.backend.common.dto.ApiResponseDto;
 import com.HUFS19.backend.common.dto.ErrorResponse;
-import com.HUFS19.backend.error.ErrorCode;
 import org.springframework.http.HttpStatus;
 
 public class ResponseUtils {
     public static <T> ApiResponseDto<T> ok(T response){
-        ApiResponseDto apiResponseDto = new ApiResponseDto();
+        ApiResponseDto<T> apiResponseDto = new ApiResponseDto<>();
         apiResponseDto.setData(response);
         apiResponseDto.setSuccess(true);
         return apiResponseDto;
     }
 
     public static <T> ApiResponseDto<T> error(HttpStatus status, int code,  String message){
-        ApiResponseDto apiResponseDto = new ApiResponseDto();
+        ApiResponseDto<T> apiResponseDto = new ApiResponseDto<>();
         apiResponseDto.setSuccess(false);
         apiResponseDto.setError(new ErrorResponse(status, code, message));
         return apiResponseDto;
