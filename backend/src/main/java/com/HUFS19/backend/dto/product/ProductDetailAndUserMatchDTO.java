948d031be7c5fdbe813b75132b37831a9adec84b
JH CHO <junh9541@gmail.com>
2024-03-29 18:12:03 +0900

    feat: 중복 product dto 정리, getProductById API 응답 데이터 변경

---
+++ backend/src/main/java/com/HUFS19/backend/dto/product/ProductDetailAndUserMatchDTO.java
@@ -0,0 +1,20 @@
+package com.HUFS19.backend.dto.product;
+
+import com.HUFS19.backend.dto.auth.LoginStatusDto;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+
+@Getter
+@Setter
+public class ProdudctDetailDto {
+    private int id;
+    private String name;
+    private String uploader;
+    private int categoryId;
+    private String detail;
+    private String mainImg;
+    private Timestamp date;
+    private LoginStatusDto loginStatus;
+}
